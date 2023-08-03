60366022b0dc1d8f9cf88bea7a161c065a3b3325
ducneuer232479 <82283267+ducneuer232479@users.noreply.github.com>
2023-08-04 03:02:37 +0700

    Update config.js

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = '<YOUR_KEY>';
+export const KEY = 'fb37cbe7-2ca7-4c35-93f6-2f0c15bf9acf';
 export const MODAL_CLOSE_SEC = 2.5;
